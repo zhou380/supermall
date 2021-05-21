51e4f9c86079707fffd75e0da21ba1a23f1ee9bf
tom_glb <goodMorning_glb@atguigu.com>
2021-05-21 10:38:59 +0800

    修改配置

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+  <div id="app">
+    11
+    <img src="assets/img/shopping.png" />
+  </div>
+</template>
 
 <script>
 export default {
@@ -8,4 +13,5 @@ export default {
 </script>
 
 <style>
+@import "assets/css/base.css";
 </style>
